90e74a45d09f8230d2ba75b3aeb639781e8c5f33
Adrian Durci <2403010d@student.gla.ac.uk>
2022-10-15 17:58:26 +0100

    bfs baseline implemented

--- adrian_bot/bfs_util.py
+++ adrian_bot/bfs_util.py
@@ -2,6 +2,8 @@
 
 
 from queue import Queue
+from re import sub
+from bot_utilities import TileType
 from game_state import GameInstance
 
 moves = [
@@ -20,19 +22,26 @@ moves_diag = [
 def add_pos(pos_a, pos_b):
     return int(pos_a[0]+pos_b[0]), int(pos_a[1]+pos_b[1])
 
-def get_target_exploring(game_instance: GameInstance):
-    pathtrace_direction = {} # from coordinate to coordinate delta 
+def sub_pos(pos_a, pos_b):
+    return int(pos_a[0]-pos_b[0]), int(pos_a[1]-pos_b[1])
+
+def get_target_exploring(game_instance: GameInstance, target_pos=None):
+    pathtrace_direction = {game_instance.player_pos: (0,0)} # from coordinate to coordinate delta
     search_queue = Queue()
     search_queue.put(game_instance.player_pos)
     
-    while search_queue:
+    while not search_queue.empty() or target_found:
         cur = search_queue.get(0)
         valid_moves = []
+        target_found = None
         for move in moves:
             new_pos = add_pos(cur, move)
             if not new_pos in pathtrace_direction:
                 pathtrace_direction[new_pos] = move
                 search_queue.put(new_pos)
+                if game_instance.get_tile_at(new_pos) == TileType.UNEXPLORED or new_pos == target_pos:
+                    target_found = new_pos
+                    break
                 valid_moves.append(True)
             else:
                 valid_moves.append(False)
@@ -44,11 +53,22 @@ def get_target_exploring(game_instance: GameInstance):
             if not new_pos in pathtrace_direction:
                 pathtrace_direction[new_pos] = move
                 search_queue.put(new_pos)
-                if 
-        
-    return 
+                if game_instance.get_tile_at(new_pos) == TileType.UNEXPLORED or new_pos == target_pos:
+                    target_found = new_pos
+                    break
+    return target_found, pathtrace_direction
 
 
+def get_move_position_from_trace(target_pos, trace):
+    cur_pos = target_pos
+    last_direction = None
+    last_straight_endpoint = cur_pos
+    while trace[cur_pos] != (0, 0):
+        if last_direction != trace[cur_pos]:
+            last_straight_endpoint = cur_pos
+        last_direction = trace[cur_pos]
+        cur_pos = sub_pos(cur_pos, last_direction)
+    return cur_pos
 
-def get_target_key(game_instance: GameInstance):
+# def get_target_key(game_instance: GameInstance):
 
